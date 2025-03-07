854597a74291e68371413c293055ece0e3302076
Gabriel Borges <gabrielborgesw2@gmail.com>
2025-03-06 23:12:14 -0300

    "teste"

--- test/server.test.ts
+++ test/server.test.ts
@@ -1,17 +1,19 @@
 // src/tests/servidor.test.ts
 import request from 'supertest';
 import { app } from '../src/index';
+import { normalize } from 'path';
 
 describe('Testando o servidor Express', () => {
   it('Deve responder com status 200 para a rota /calcular-paletes', async () => {
     const response = await request(app)
       .post('/calcular-paletes')
       .send({
-        tamanhoLocal: 100,
-        tamanhoPalet: 10,
-        espacoCorredor: 10,
-        espacoSaida: 5,
-        espacoPicking: 5,
+        tamanhoLocal: 3000,
+        tamanhoPalet: 2,
+        espacoCorredor: 450,
+        espacoSaida: 300,
+        espacoPicking: 150,
+        nomeProduto: 'Produto A',
       });
     expect(response.status).toBe(200);
     expect(response.body.quantidadePaletes).toBe(8); // Espera-se que caibam 8 paletes
@@ -21,10 +23,11 @@ describe('Testando o servidor Express', () => {
     const response = await request(app)
       .post('/calcular-paletes')
       .send({
-        tamanhoLocal: 100,
-        tamanhoPalet: 10,
-        espacoCorredor: 10,
-        espacoPicking: 5,
+        tamanhoLocal: 3000,
+        tamanhoPalet: 450,
+        espacoCorredor: 300,
+        espacoPicking: 150,
+        nomeProduto: 'Produto A',
       });
     expect(response.status).toBe(400); // Espera-se erro 400 devido à falta de espacoSaida
   });
